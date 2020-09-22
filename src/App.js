8cde991fbd52cc97713f868a798a194005f33c5d
lokesh234 <lgangaramaney@wpi.edu>
2020-09-21 23:21:04 -0400

    Completed Climate Stories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,168 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, Fragment, useState} from 'react';
+import {Map, Marker, TileLayer, Popup} from "react-leaflet";
+import { Sidebar, Tab } from 'react-leaflet-sidetabs';
+import { FiChevronRight } from "react-icons/fi";
+import { FaMapMarkedAlt } from "react-icons/fa";
+import WomenInTheWorld from "./Components/WomenInTheWorldData";
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import MaterialTable from "material-table";
+import { Button } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import projectCenters from "./Components/IQPLocations";
+import colnames from "./Components/Column";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+var popup = L.popup();
+
+let markerLayers = {};
+
+let currentCountry;
+
+
+var WCenters = [];
+let WCenterInfo = [];
+let sponsorInfo = {};
+let filteredInfo = {};
+let currentInfo = {};
+
+
+
+
+function FindingProjectCenters() {
+  projectCenters.forEach(function (key) {
+    WomenInTheWorld.forEach(function (Center) {
+      if (key.name === Center.ProjectCenter){
+        WCenters.push(key);
+        WCenterInfo.push(Center);
+      }
+    });
+  })
 }
 
-export default App;
+FindingProjectCenters();
+let uniqueCenters = [...new Set(WCenters)]
+
+function showPopup(){
+  this.openPopup();
+}
+
+function hidePopup(){
+  this.closePopup();
+}
+
+
+export default class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: true,
+      selected: 'home',
+    };
+  }
+
+  onClose() {
+    this.setState({collapsed: true});
+  }
+  onOpen(id) {
+    this.setState({
+      collapsed: false,
+      selected: id,
+    })
+  }
+
+  handleClick = (country) => (e) => {
+    WCenterInfo = [];
+    WomenInTheWorld.forEach(function (Center) {
+      if (country === Center.ProjectCenter){
+        WCenterInfo.push(Center);
+      }
+    });
+    this.onOpen("settings");
+  }
+
+  handleButtonClick = () => (e) => {
+    WCenterInfo = [];
+    projectCenters.forEach(function (key) {
+      WomenInTheWorld.forEach(function (Center) {
+        if (key.name === Center.ProjectCenter){
+          WCenterInfo.push(Center);
+        }
+      });
+    })
+    this.onOpen("settings");
+  }
+
+  render() {
+    return (
+        <Fragment>
+          <Sidebar
+              id="sidebar"
+              position="right"
+              collapsed={this.state.collapsed}
+              closeIcon={<FiChevronRight />}
+              selected={this.state.selected}
+              onOpen={this.onOpen.bind(this)}
+              onClose={this.onClose.bind(this)}
+          >
+            <Tab id="settings" header="Project Centers" icon={<FaMapMarkedAlt />}>
+              <div>
+                <Box mt={2} />
+                <MaterialTable title="IQPs"
+                               columns={colnames.map((c) => ({ ...c, tableData: undefined }))}
+                               data={WCenterInfo}
+                               options={{
+                                 search: true,
+                                 exportButton: true,
+                                 pageSize:5,
+                                 pageSizeOptions: [10]
+                               }}/>
+                <Box mt={2} />
+                <Button style={{
+                  backgroundColor: "#0074d9",
+                }}
+                        onClick={this.handleButtonClick()}
+                        variant="contained" color="primary">
+                  Clear
+                </Button>
+              </div>
+            </Tab>
+          </Sidebar>
+          <Map style={{ height: "100vh", width: "100%" }} className="mapStyle" center={[0, 0]} zoom={3}>
+            <TileLayer
+                attribution='Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+                url={'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}'}
+            />
+            {uniqueCenters.map((center, k) => {
+              let position = [center["coordinates"][0], center["coordinates"][1]]
+              return (
+                  <Marker
+                      key={k}
+                      onMouseOver={(e) => {
+                        e.target.openPopup();
+                      }}
+                      onMouseOut={(e) => {
+                        e.target.closePopup();
+                      }}
+                      onClick={this.handleClick(center.name)}
+                      position={position}
+                  >
+                    <Popup> {center.name} </Popup>
+                  </Marker>
+              )
+            })
+            }
+          </Map>
+        </Fragment>
+    )
+  }
+
+}
---
+++ src/Components/WomenInTheWorldData.js
@@ -0,0 +1,94 @@
+import React from "react";
+
+const WomenInTheWorld = [
+    {
+        "Title": "Perceptions Of A Changing Climate, Exposure and Vulnerability",
+        "ProjectCenter": "Mandi, India Project Center - IQP",
+        "Project Type": "IQP",
+        "Link": "https://digitalcommons.wpi.edu/iqp-all/3263/?_ga=2.232170525.300681655.1569780222-1077499511.1538148867",
+        "Date": "5/1/2018",
+        "Sponsor": "IIT-Mandi"
+    },
+    {
+        "Title": "Stories of Climate Change",
+        "ProjectCenter": "Mandi, India Project Center - IQP",
+        "Project Type": "IQP",
+        "Link": "https://digital.wpi.edu/concern/student_works/p2676x96v?locale=en",
+        "Date": "4/28/2019",
+        "Sponsor": "IIT-Mandi"
+    },
+    {
+        "Title": "Perceptions of Climate Change",
+        "ProjectCenter": "Reykjavik, Iceland Project Center - IQP",
+        "Project Type": "IQP",
+        "Link": "https://digitalcommons.wpi.edu/iqp-all/5142/?_ga=2.219580571.300681655.1569780222-1077499511.1538148867",
+        "Date": "10/11/2018",
+        "Sponsor": "WPI"
+    },
+    {
+        "Title": "Climate Change Identities",
+        "ProjectCenter": "Wellington, New Zealand Project Center - IQP",
+        "Project Type": "IQP",
+        "Link": "https://digitalcommons.wpi.edu/cgi/viewcontent.cgi?article=1238&context=iqp-all&_ga=2.219580571.300681655.1569780222-1077499511.1538148867",
+        "Date": "3/6/2014",
+        "Sponsor": "Landcare Research"
+    },
+    {
+        "Title": "Investigating Flood and Climate Change Perception",
+        "ProjectCenter": "Wellington, New Zealand Project Center - IQP",
+        "Project Type": "IQP",
+        "Link": "https://digitalcommons.wpi.edu/iqp-all/740/?_ga=2.256214281.300681655.1569780222-1077499511.1538148867",
+        "Date": "3/2/2015",
+        "Sponsor": "Greater Wellington Regional Council"
+    },
+    {
+        "Title": "Improving Science Communication In Wellington: An Analysis Of Public Knowledge of Sea Level Rise In The Central Business District of Wellington, New Zealand",
+        "ProjectCenter": "Wellington, New Zealand Project Center - IQP",
+        "Project Type": "IQP",
+        "Link": "http://https//digitalcommons.wpi.edu/iqp-all/1377/",
+        "Date": "3/2/2018",
+        "Sponsor": "Antarctic Research Center"
+    },
+    {
+        "Title": "Ethnographic Film Study of Indonesian Culture and Climate",
+        "ProjectCenter": "Melbourne, Australia Project Center - IQP",
+        "Project Type": "IQP",
+        "Link": "https://digitalcommons.wpi.edu/iqp-all/5426/",
+        "Date": "4/30/2019",
+        "Sponsor": "CERES"
+    },
+    {
+        "Title": "Storying Perceptions of Climate Change in Wellington Shire",
+        "ProjectCenter": "Melbourne, Australia Project Center - IQP",
+        "Project Type": "IQP",
+        "Link": "https://digital.wpi.edu/concern/student_works/fn107133t?locale=en",
+        "Date": "3/5/2020",
+        "Sponsor": "Ingrid Shockey"
+    },
+    {
+        "Title": "Climate Stories as a Means of Increasing Public Participation in Climate Change Adaptation in Albania",
+        "ProjectCenter": "Tirana, Albania Project Center - IQP",
+        "Project Type": "IQP",
+        "Link": "https://digital.wpi.edu/concern/student_works/nv935512d?locale=en",
+        "Date": "12/11/2019",
+        "Sponsor": "GIZ"
+    },
+    {
+        "Title": "Storying Climate Change: London",
+        "ProjectCenter": "London, England Project Center - IQP",
+        "Project Type": "IQP",
+        "Link": "https://digital.wpi.edu/concern/student_works/wd375z582?locale=en",
+        "Date": "3/4/2020",
+        "Sponsor": "Ingrid Shockey"
+    },
+    {
+        "Title": "Perceptions of Climate Change in Japan",
+        "ProjectCenter": "Japan Project Center (Kyoto) - IQP",
+        "Project Type": "IQP",
+        "Link": "https://digital.wpi.edu/concern/student_works/3n2041510?locale=en",
+        "Date": "10/8/2019",
+        "Sponsor": "WPI"
+    }
+]
+
+export default WomenInTheWorld
