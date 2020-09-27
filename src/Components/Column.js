7c4853f1b2cc9343fe2699f7ddb73c76fd00bf2e
lokesh234 <lgangaramaney@wpi.edu>
2020-09-27 12:07:55 -0400

    Made the links hyperlinks

--- src/Components/Column.js
+++ src/Components/Column.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './styles.css'
 
 
 const colnames = [
@@ -15,7 +16,8 @@ const colnames = [
         title: 'Date', field: 'Date'
     },
     {
-        title: 'Link', field: 'Link'
+        title: 'Link', field: 'Link',
+        render: rowData => <a href={rowData.Link} target="_blank">{rowData.Link}</a>,
     }
 ]
 
